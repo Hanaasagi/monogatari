2a7e28db04fd30c2dfe6b5998a8c1d197f5f2344
Qsaka <949241796@qq.com>
2016-01-19 08:27:30 +0800

    about me

---
+++ about.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>关于我</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-combined.min.css">
+    <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <style type="text/css">
+        body {
+            background-image:url("image/theme.png");
+            background-size: cover;
+            background-repeat:no-repeat;
+            background-attachment:fixed;
+        }
+        .pagefont{
+            color:#A8A8A8;
+        }
+        .jumbotron {
+            background:url("image/banner.jpg");
+            background-position: 50% 60%;
+            background-size: cover;
+            box-shadow: 0 0px 30px rgba(0, 0, 0, 0.36) inset;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span2"></div>
+            <div class="span8">
+                <div class="header">
+                    <ul class="nav nav-pills pull-right">
+                        <li>
+                            <a href="./index.php">主页</a>
+                        </li>
+                        <li>
+                            <a href="./archives.php">归档</a>
+                        </li>
+                        <li>
+                            <a href="./about.php">about me</a>
+                        </li>
+                    </ul>
+                    <h4 class="text-muted">
+                        <small>Qsaka</small>
+                    </h4>
+                </div>
+                <div class="jumbotron">
+                    <p class="lead pull-middle"> 
+                        我們所度過的每個平凡の日常
+                    </p>
+                    <div class="span12"></div>
+                    <div class="span1"></div>
+                    <h2>
+                        也許就是連續發生的奇跡 
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <!-- 简介 -->
+        <div class="row-fluid">
+            <div class="span2"></div>
+            <div class="span8 pagefont">
+                <h2>
+                    关于我
+                </h2>
+                <p>
+                    这个人很懒，什么都没有留下……
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
+
+  
